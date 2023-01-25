fb837b67dc651e039bad12e0a998ae068bb29938
Harinder daksh <81361432+harrydaksh@users.noreply.github.com>
2023-01-25 14:45:03 +0530

    second

--- src/data.js
+++ src/data.js
@@ -1,37 +1,42 @@
+import img from '../src/assets/Subject.png'
+import img2 from '../src/assets/Subject 2.png'
+import img3 from '../src/assets/Subject 3.png'
+import img4 from '../src/assets/Subject 4.png'
+import img5 from '../src/assets/Subject 5.png'
 export default [
   {
     id: 1,
     name: 'Bertie Yates',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    img
   },
   {
     id: 2,
     name: 'Hester Hogan',
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    img2
   },
   {
     id: 3,
     name: 'Larry Little',
     age: 36,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    img3
   },
   {
     id: 4,
     name: 'Sean Walsh',
     age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    img4
   },
   {
     id: 5,
     name: 'Lola Gardner',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    img5
   },
 ];
